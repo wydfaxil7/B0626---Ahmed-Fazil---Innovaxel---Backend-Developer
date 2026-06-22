27919e39e66c3b6dc3e8984191a5b372943896aa
wydfaxil7 <a.faazil07@gmail.com>
2026-06-22 18:59:21 +0500

    add events enpoints -create and list with filtering and sorting too..

---
+++ routers/events.py
@@ -0,0 +1,73 @@
+from fastapi import APIRouter, Depends, HTTPException, Query
+from sqlalchemy.orm import Session
+from database import get_db
+from models import Event, Registration
+from schemas import EventCreate, EventResponse
+from datetime import datetime,timezone
+
+router = APIRouter(
+    prefix="/events",
+    tags=["Events"]
+)
+
+def build_event_response(event: Event) -> EventResponse:
+    
+    active_registrations = [
+        r for r in event.registration if r.status == "active" 
+    ] 
+    total_registrations = len(active_registrations)
+    available_seats = event.total_seats - total_registrations
+
+    return EventResponse(
+        id=event.id
+        name=event.name,
+        total_seats=event.total_seats,
+        event_date=event.event_date,
+        created_at=event.created_at,
+        available_seats=available_seats,
+        total_registrations=total_registrations
+    )
+
+
+@router.post("/", response_model=EventResponse, status_code=201)
+def create_event(
+    event_data: EventCreate, 
+    db: Session = Depends(get_db)
+):
+
+    existing = db.query(Event).filter(Event.name == event_date.name).first()
+    if existing:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Event with name '{event_data.name}' already exists."
+        )
+
+    event_date = event_data.event_date.replace(tzinfo=None)
+
+    new_event = Event(
+        name=event_data.name,
+        total_seats=event_data.total_seats,
+        event_date=event_date
+    )
+
+    db.add(new_event)
+    db.commit()
+    db.refresh(new_event)
+
+    return build_event_response(new_event)
+
+
+@router.get("/", response_model=list[EventResponse])
+def get_events(
+    upcoming_only: bool = Query(False, description="Filter to show only future events"),
+    db: Session = Depends(get_db)
+):
+    query = db.query(Event)
+
+    if upcoming_only:
+        now = datetime.now(timezone.utc).replace(tzinfo=None)
+        query = query.filter(Event.event_date > now)
+
+    events = query.order_by(Event.event_date.asc()).all()
+
+    return [build_event_response(event) for event in events]
